2c7153f08ca7af5596f072001cba110ac7ae3b26
Bakhr1ddin <bakhr1dd1n17@gmail.com>
2021-09-12 12:44:16 +0500

    Add scrollbar and fixed bugs, Update

---
+++ scripts/scrollbar.js
@@ -0,0 +1,7 @@
+$(window).scroll(function () {
+  let scroll = $(window).scrollTop(),
+    dh = $(document).height(),
+    wh = $(window).height();
+  scrollPercent = (scroll / (dh - wh)) * 100;
+  $("#progressbar").css("height", scrollPercent + "%");
+});
